957d8394c845c56913aaba66f7ae3b8ed3666486
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-07-10 02:36:52 +0700

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -128,7 +128,7 @@ class MyApp extends StatelessWidget {
               return MaterialPageRoute(builder: (_) {
                 return Scaffold(
                   body: Center(
-                    child: Text('dont have a page'),
+                    child: Text('Page not found :('),
                   ),
                 );
               });
